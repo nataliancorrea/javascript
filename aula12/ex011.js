ce0a24fc58b63f02df9168bf12ed89f20634c809
Natália Corrêa <131316033+nataliancorrea@users.noreply.github.com>
2023-10-22 17:49:17 -0300

    realizado exercicios da aula 12

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 25
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16){
+    console.log('Você não vota!')
+} else if (idade < 18  || idade >= 65){
+    console.log('Voto é opcional')
+} else  {
+    console.log('Voto é obrigatório!')
+}
